2b941cef9c8ac401d2f06bb26d558aeed3c7976f
Roman Zidan Ramadhan <roman.zidan.ramadhan@gmail.com>
2020-04-01 12:04:46 +0800

    Upload project Vue

---
+++ src/components/HeroDomn.vue
@@ -0,0 +1,53 @@
+<template>
+    <!-- Hero Section Begin -->
+    <section class="hero-section">
+        <carousel class="hero-items" :items="1" :nav="false" :dots="false" :autoplay="true">
+
+            <div class="single-hero-items set-bg" style="background-image: url('/img/cloth1.jpg');">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-5">
+                            <span>Cloth, Boys</span>
+                            <h1>Black friday</h1>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                                incididunt ut labore
+                                et dolore
+                            </p>
+                            <a href="#" class="primary-btn">Shop Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="single-hero-items set-bg" style="background-image: url('/img/cloth1.jpg');">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-5">
+                            <span>Cloth, Boys</span>
+                            <h1>Black friday</h1>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                                incididunt ut labore
+                                et dolore
+                            </p>
+                            <a href="#" class="primary-btn">Shop Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </carousel>
+    </section>
+    <!-- Hero Section End -->
+</template>
+
+<script>
+import carousel from 'vue-owl-carousel';
+
+export default {
+    name : 'HeroDomn',
+    components : {
+        carousel
+    }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+
+    <HeaderDomn />
+
+    <HeroDomn/>
+
+    <BannerDomn/>
+
+    <InstaDomn/>
+
+    <PartnerDomn/>
+
+    <FooterDomn/>
+
+  </div>
+</template>
+
+<script>
+  import HeaderDomn from '@/components/HeaderDomn.vue';
+  import HeroDomn from '@/components/HeroDomn.vue';
+  import BannerDomn from '@/components/BannerDomn.vue';
+  import InstaDomn from '@/components/InstaDomn.vue';
+  import PartnerDomn from '@/components/PartnerDomn.vue';
+  import FooterDomn from '@/components/FooterDomn.vue';
+
+  export default {
+    name: 'Home',
+    components: {
+      HeaderDomn,
+      HeroDomn,
+      BannerDomn,
+      InstaDomn,
+      PartnerDomn,
+      FooterDomn
+    }
+  }
+</script>
